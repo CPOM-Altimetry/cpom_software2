140079c90242040c931f5d58c8d4a1d2f0788943
Alan Muir <a.muir@ucl.ac.uk>
2024-08-01 15:43:39 +0100

    added altimetry/tools/plot_netcdf.py

---
+++ src/cpom/altimetry/tools/plot_netcdf.py
@@ -0,0 +1,15 @@
+"""cpom.altimetry.tools.plot_netcdf.py
+"""
+
+
+def main():
+    """main function of tool
+
+    Returns:
+        None
+    """
+    print("hello")
+
+
+if __name__ == "__main__":
+    main()
