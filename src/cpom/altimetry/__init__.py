473a8a2732ea53541a30e726748bbf9831790424
Alan Muir <a.muir@ucl.ac.uk>
2024-08-06 17:30:57 +0100

    docs update

--- src/cpom/altimetry/__init__.py
+++ src/cpom/altimetry/__init__.py
@@ -2,7 +2,10 @@
 # Altimetry related directories
 
 - `cpom.altimetry.missions`
+    - code for the altimetry Mission class (stores info and 
+      generic functions for a range of altimetry missions)
 - `cpom.altimetry.projects`
+    - contains sub-directories of code for specific projects (ESA or other)
 - `cpom.altimetry.tools`
     - contains executable tools for various tasks
 """
