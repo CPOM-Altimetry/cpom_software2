0d485f499aac7c2f4db6613ad0fc2cf4f400e752
Alan Muir <a.muir@ucl.ac.uk>
2024-08-13 14:51:18 +0100

    docs updated

--- src/cpom/roughness/__init__.py
+++ src/cpom/roughness/__init__.py
@@ -2,4 +2,11 @@
 
 # Class to read/interpolate roughness data
 
+## Supported Roughness Data Sets
+
+- rema_100m_900ws_roughness_zarr 
+  (Roughness calculated from REMA 100m, J.Phillips 2024)
+- arcticdem_100m_900ws_roughness_zarr 
+  (Roughness calculated from ArcticDEM 100m, J.Phillips 2024)
+
 """
