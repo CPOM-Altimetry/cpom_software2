44c1b418f9819bedfb79da32e00e80e64996e905
Alan Muir <a.muir@ucl.ac.uk>
2024-08-20 17:52:49 +0100

    plot_waveforms.py updated to correct pylinting issue

--- src/cpom/altimetry/tools/plot_waveforms.py
+++ src/cpom/altimetry/tools/plot_waveforms.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-""" cpom.altimetry.tools.plot_waveforms.py
+"""cpom.altimetry.tools.plot_waveforms.py
 
-# Tool to plot altimetry waveforms
+# Tool to plot altimetry waveforms. Directly supports:
+
+CRISTAL L1b
+CS2 L1b
+
++ any NetCDF file that has selectable latitude, longitude and waveform parameters
 
 """
+
 import argparse
 import os
 import sys
@@ -13,6 +19,7 @@ import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import matplotlib.pyplot as plt
 import numpy as np
+from cartopy.mpl.geoaxes import GeoAxes
 from matplotlib.widgets import Button
 from netCDF4 import Dataset, Variable  # pylint: disable=no-name-in-module
 
@@ -52,7 +59,7 @@ def get_projection_and_extent(lat: float) -> tuple[ccrs.Projection, list[float] 
         tuple[ccrs.Projection, list[float] | None]: The Cartopy projection and the extent.
     """
     if -50 <= lat <= 50:
-        return ccrs.Mercator(), None
+        return ccrs.Mercator(), [-179.9, 179.9, -90, 90]
     if lat > 50:
         return ccrs.Stereographic(central_latitude=90, central_longitude=0), [-180, 180, 50, 90]
 
@@ -60,7 +67,7 @@ def get_projection_and_extent(lat: float) -> tuple[ccrs.Projection, list[float] 
 
 
 def update_map(
-    ax: plt.Axes,
+    ax: GeoAxes,
     current_lat: float,
     current_lon: float,
     latitudes: np.ndarray,
@@ -75,7 +82,6 @@ def update_map(
         current_lon (float): The current longitude of the waveform.
         latitudes (np.ndarray): Array of latitude values for the track.
         longitudes (np.ndarray): Array of longitude values for the track.
-        projection (ccrs.Projection): The Cartopy projection to use.
         extent (list[float] | None, optional): The extent for the map projection. Defaults to None.
     """
     ax.clear()
@@ -110,7 +116,7 @@ def update_map(
 def update_plot(
     index: int,
     ax_waveform: plt.Axes,
-    ax_map: plt.Axes,
+    ax_map: GeoAxes,
     waveforms: np.ndarray,
     latitudes: np.ndarray,
     longitudes: np.ndarray,
@@ -162,7 +168,7 @@ def update_plot(
     fig.canvas.draw_idle()
 
 
-def get_default_names(
+def get_default_parameter_names(
     file_name: str, waveform_parameter_name: str | None, latname: str | None, lonname: str | None
 ) -> tuple[str, str, str]:
     """Get default parameter names for specific file types if not provided.
@@ -174,7 +180,8 @@ def get_default_names(
         lonname (str | None): The longitude parameter name.
 
     Returns:
-        tuple[str, str, str]: The waveform parameter name, latitude name, and longitude name.
+        tuple[str, str, str,str]: The waveform parameter name,
+        latitude name,longitude name.
     """
 
     wf_parameter = ""
@@ -190,6 +197,15 @@ def get_default_names(
             latitude_name = "data/ku/latitude"
         if lonname is None:
             longitude_name = "data/ku/longitude"
+    # Check if it is a CRISTAL L1b file in LR mode
+    elif "CRA_IR_1B_LR__" in file_name[: len("CRA_IR_1B_LR__")]:
+        print("CRISTAL l1b (CRA_IR_1B_LR__) identified")
+        if waveform_parameter_name is None:
+            wf_parameter = "data_20/ku/power_waveform_comb"
+        if latname is None:
+            latitude_name = "data_20/ku/latitude"
+        if lonname is None:
+            longitude_name = "data_20/ku/longitude"
     elif "SIR_LRM_1B" in file_name[8 : 8 + len("SIR_LRM_1B")]:
         print("CryoSat-2 LRM L1b file identified")
         if waveform_parameter_name is None:
@@ -225,6 +241,53 @@ def get_default_names(
     return wf_parameter, latitude_name, longitude_name
 
 
+def get_scale_factor(nc: Dataset, file_name: str, wf_name: str, waveforms: np.ndarray):
+    """retrieve an array of scale factors to convert waveform counts to Watts
+    This is mission dependant
+
+    Args:
+        nc (Dataset):netcdf dataset
+        file_name (str): file name
+        wf_name (str): name of waveform parameter
+    """
+
+    if "CRA_IR_1B_HR__" in file_name[: len("CRA_IR_1B_HR__")]:
+        if wf_name == "data/ku/power_waveform_comb":
+            scale_factor = get_variable(nc, "data/ku/waveform_scale_factor_comb")[:].data
+        elif wf_name == "data/ku/power_waveform_rx1":
+            scale_factor = get_variable(nc, "data/ku/waveform_scale_factor_rx1")[:].data
+        elif wf_name == "data/ku/power_waveform_rx2":
+            scale_factor = get_variable(nc, "data/ku/waveform_scale_factor_rx2")[:].data
+        elif wf_name == "data/ka/power_waveform_comb":
+            scale_factor = get_variable(nc, "data/ka/waveform_scale_factor_comb")[:].data
+        elif wf_name == "data/ka/power_waveform_rx1":
+            scale_factor = get_variable(nc, "data/ka/waveform_scale_factor_rx1")[:].data
+        elif wf_name == "data/ka/power_waveform_rx2":
+            scale_factor = get_variable(nc, "data/ka/waveform_scale_factor_rx2")[:].data
+        else:
+            raise ValueError("wf parameter not supported")
+
+    elif "CRA_IR_1B_LR__" in file_name[: len("CRA_IR_1B_LR__")]:
+        if wf_name == "data_20/ku/power_waveform_comb":
+            scale_factor = get_variable(nc, "data_20/ku/waveform_scale_factor_comb")[:].data
+        elif wf_name == "data_20/ku/power_waveform_rx1":
+            scale_factor = get_variable(nc, "data_20/ku/waveform_scale_factor_rx1")[:].data
+        elif wf_name == "data_20/ku/power_waveform_rx2":
+            scale_factor = get_variable(nc, "data_20/ku/waveform_scale_factor_rx2")[:].data
+        elif wf_name == "data_20/ka/power_waveform_comb":
+            scale_factor = get_variable(nc, "data_20/ka/waveform_scale_factor_comb")[:].data
+        elif wf_name == "data_20/ka/power_waveform_rx1":
+            scale_factor = get_variable(nc, "data_20/ka/waveform_scale_factor_rx1")[:].data
+        elif wf_name == "data_20/ka/power_waveform_rx2":
+            scale_factor = get_variable(nc, "data_20/ka/waveform_scale_factor_rx2")[:].data
+        else:
+            raise ValueError("wf parameter not supported")
+    else:
+        scale_factor = np.ones(np.shape(waveforms)[0], dtype=float)
+
+    return scale_factor
+
+
 def get_variable(nc: Dataset, nc_var_path: str) -> Variable:
     """Retrieve variable from NetCDF file, handling groups if necessary.
 
@@ -247,7 +310,7 @@ def get_variable(nc: Dataset, nc_var_path: str) -> Variable:
         try:
             var = var[part]
         except (KeyError, IndexError):
-            sys.exit(f"NetCDF parameter '{nc_var_path}' not found")
+            sys.exit(f"NetCDF parameter '{nc_var_path}' not found in file")
     return var
 
 
@@ -257,7 +320,16 @@ def main(cmd_args: list[str]) -> None:
     Args:
         args (list[str]): List of command-line arguments passed to the script.
     """
-    parser = argparse.ArgumentParser(description="Tool to plot altimetry waveforms")
+    parser = argparse.ArgumentParser(
+        description=(
+            "Tool to plot altimetry waveforms and show"
+            " the location of waveform and track on a map. "
+            "For CS2 and CRISTAL L1b files, default parameters"
+            " will be used for waveforms and latitude "
+            "and longitude unless specified with command line"
+            " options."
+        )
+    )
     parser.add_argument(
         "--file", "-f", help=("file name of NetCDF file to plot waveforms from"), required=False
     )
@@ -287,6 +359,21 @@ def main(cmd_args: list[str]) -> None:
         type=int,
         default=1,
     )
+    parser.add_argument(
+        "--delay",
+        "-d",
+        help=("[optional, float, def=0.2] : delay between waveforms when animating"),
+        required=False,
+        type=float,
+        default=0.2,
+    )
+    parser.add_argument(
+        "--scale_to_watts",
+        "-w",
+        help=("[optional] : if set, scale to watts"),
+        required=False,
+        action="store_true",
+    )
     args = parser.parse_args(cmd_args)
 
     if not args.file:
@@ -294,25 +381,22 @@ def main(cmd_args: list[str]) -> None:
 
     file_name = os.path.basename(args.file)
 
-    wf_name, latitude_name, longitude_name = get_default_names(
+    wf_name, latitude_name, longitude_name = get_default_parameter_names(
         file_name, args.parameter, args.latname, args.lonname
     )
 
     with Dataset(args.file) as nc:
-        variable = get_variable(nc, wf_name)
-        waveforms = variable[:].data
+        waveforms = get_variable(nc, wf_name)[:].data
+        scale_factor = get_scale_factor(nc, file_name, wf_name, waveforms)
 
-        lat_path = latitude_name.split("/")
-        lon_path = longitude_name.split("/")
-        latitudes = nc
-        longitudes = nc
-        for group in lat_path:
-            latitudes = latitudes[group]
-        for group in lon_path:
-            longitudes = longitudes[group]
-
-        latitudes = latitudes[:]
-        longitudes = longitudes[:]
+        if args.scale_to_watts:
+            waveforms_scaled = waveforms * scale_factor[:, np.newaxis]
+            units = "watts"
+        else:
+            waveforms_scaled = waveforms
+            units = "counts"
+        latitudes = get_variable(nc, latitude_name)[:].data
+        longitudes = get_variable(nc, longitude_name)[:].data
 
     fig = plt.figure(figsize=(15, 8))
     gs = fig.add_gridspec(1, 2, width_ratios=[2, 1], wspace=0.05)
@@ -321,23 +405,26 @@ def main(cmd_args: list[str]) -> None:
 
     plt.subplots_adjust(left=0.1, right=0.95, bottom=0.25, top=0.9)
 
-    if args.index > len(waveforms) - 1:
+    if args.index > len(waveforms_scaled) - 1:
         sys.exit("--index <WAVEFORM_INDEX> number is > (number of waveforms in file-1)")
     current_index = args.index - 1
 
+    animating = False  # Flag to track animation state
+    animation_delay = args.delay
+
     def next_waveform(_: object) -> None:
         nonlocal current_index
-        if current_index < len(waveforms) - 1:
+        if current_index < len(waveforms_scaled) - 1:
             current_index += 1
             update_plot(
                 current_index,
                 ax_waveform,
                 ax_map,
-                waveforms,
+                waveforms_scaled,
                 latitudes,
                 longitudes,
                 file_name,
-                wf_name,
+                wf_name + f"({units})",
                 fig,
             )
 
@@ -349,11 +436,11 @@ def main(cmd_args: list[str]) -> None:
                 current_index,
                 ax_waveform,
                 ax_map,
-                waveforms,
+                waveforms_scaled,
                 latitudes,
                 longitudes,
                 file_name,
-                wf_name,
+                wf_name + f"({units})",
                 fig,
             )
 
@@ -364,72 +451,102 @@ def main(cmd_args: list[str]) -> None:
             current_index,
             ax_waveform,
             ax_map,
-            waveforms,
+            waveforms_scaled,
             latitudes,
             longitudes,
             file_name,
-            wf_name,
+            wf_name + f"({units})",
             fig,
         )
 
     def forward_to_end(_: object) -> None:
         nonlocal current_index
-        current_index = len(waveforms) - 1
+        current_index = len(waveforms_scaled) - 1
         update_plot(
             current_index,
             ax_waveform,
             ax_map,
-            waveforms,
+            waveforms_scaled,
             latitudes,
             longitudes,
             file_name,
-            wf_name,
+            wf_name + f"({units})",
             fig,
         )
 
     def quit_program(_: object) -> None:
-        plt.close(fig)
+        sys.exit(0)
+
+    def animate_waveforms(_: object) -> None:
+        nonlocal current_index, animating, animation_delay
+        if animating:
+            animating = False  # Stop animation
+            banimate.label.set_text("Animate >|")
+            fig.canvas.draw_idle()  # Force update of the button label
+        else:
+            animating = True  # Start animation
+            banimate.label.set_text("Stop")
+            fig.canvas.draw_idle()  # Force update of the button label
+            while animating and current_index < len(waveforms_scaled) - 1:
+                current_index += 1
+                update_plot(
+                    current_index,
+                    ax_waveform,
+                    ax_map,
+                    waveforms_scaled,
+                    latitudes,
+                    longitudes,
+                    file_name,
+                    wf_name + f"({units})",
+                    fig,
+                )
+                plt.pause(animation_delay)  # Short delay between frames
 
     button_width = 0.1
     button_height = 0.075
     button_padding = 0.02
-    start_x = (1 - 5 * (button_width + button_padding) + button_padding) / 2
+    start_x = (1 - 6 * (button_width + button_padding) + button_padding) / 2
 
-    axrewind = plt.axes([start_x, 0.05, button_width, button_height])
+    axrewind = plt.axes((start_x, 0.05, button_width, button_height))
     axprev = plt.axes(
-        [start_x + (button_width + button_padding), 0.05, button_width, button_height]
+        (start_x + (button_width + button_padding), 0.05, button_width, button_height)
     )
     axnext = plt.axes(
-        [start_x + 2 * (button_width + button_padding), 0.05, button_width, button_height]
+        (start_x + 2 * (button_width + button_padding), 0.05, button_width, button_height)
     )
     axforward = plt.axes(
-        [start_x + 3 * (button_width + button_padding), 0.05, button_width, button_height]
+        (start_x + 3 * (button_width + button_padding), 0.05, button_width, button_height)
+    )
+    axanimate = plt.axes(
+        (start_x + 4 * (button_width + button_padding), 0.05, button_width, button_height)
     )
     axquit = plt.axes(
-        [start_x + 4 * (button_width + button_padding), 0.05, button_width, button_height]
+        (start_x + 5 * (button_width + button_padding), 0.05, button_width, button_height)
     )
 
     brewind = Button(axrewind, "|<")
     bprev = Button(axprev, "Previous")
     bnext = Button(axnext, "Next")
     bforward = Button(axforward, ">|")
+    banimate = Button(axanimate, "Animate >|")
     bquit = Button(axquit, "Quit")
 
     brewind.on_clicked(rewind_waveform)
     bprev.on_clicked(prev_waveform)
     bnext.on_clicked(next_waveform)
     bforward.on_clicked(forward_to_end)
+    banimate.on_clicked(animate_waveforms)
     bquit.on_clicked(quit_program)
 
     update_plot(
         current_index,
         ax_waveform,
         ax_map,
-        waveforms,
+        waveforms_scaled,
         latitudes,
         longitudes,
         file_name,
-        wf_name,
+        wf_name + f"({units})",
         fig,
     )
 
