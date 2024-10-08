cc9ef7b5fa33ae333c78a08ef95828b5a21da88d
Alan Muir <a.muir@ucl.ac.uk>
2024-10-08 09:43:39 +0100

    added src/cpom/gridding

---
+++ src/cpom/gridding/gridareas.py
@@ -0,0 +1,366 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Class to define rectangular polar stereo grid areas for processing output. Does not define the
+grids internal spacing.
+
+Each area is defined by projection, bottom left corner x,y coordinates in m, width and height in m
+
+Author: Alan Muir (CPOM/UCL)
+Date: 2019
+Copyright: UCL/MSSL/CPOM. Not to be used outside CPOM/MSSL without permission of author 
+
+History:
+Updated 24/09/21 by Lin Gilbert to use pyproj CRS rather than Proj
+"""
+import numpy as np
+from pyproj import CRS, Transformer  # co-ord transforms and definitions
+
+# pylint: disable=too-many-branches,too-many-locals,too-many-statements
+# pylint: disable=too-many-instance-attributes
+
+all_grid_areas = [
+    "antarctica",
+    "greenland",
+    "antarctic_ocean",
+    "greenland_wider_area",
+    "greenland_centre",
+    "amundsen_sea_area",
+    "spirit_all",
+    "ross_1600km",
+    "filchner_1300km",
+    "arctic_russia",
+    "svalbard",
+    "novaya_zemlya",
+    "franz_josef_land",
+    "severnaya_zemlya",
+    "arctic",
+    "chongtar",
+]
+
+
+class GridArea:
+    """class to define and use named grid areas for polar regions"""
+
+    def __init__(self, name: str, binsize: int):
+        """initialization function for GridArea class
+
+        Args:
+            name (str): GridArea area name
+            binsize (int): bin size of grid to use in m
+        """
+        self.name = name
+        self.binsize = binsize  # bin width in meters
+        self.halfbinsize = binsize / 2
+
+        self.crs_wgs = CRS("epsg:4326")  # ellipsoid : WGS84
+
+        if name == "antarctica":
+            self.long_name = "Antarctica"
+            self.minxm = -2820e3  # bottom left corner x coordinate of grid in m
+            self.minym = -2420e3  # bottom left corner y coordinate of grid in m
+            self.grid_x_size = 5640e3  # Width of grid in integer m
+            self.grid_y_size = 4840e3  # Width of grid in integer m
+            # For lat/lon -> x,y transforms for each area
+            self.coordinate_reference_system = (
+                "epsg:3031"  # WGS 84 / Antarctic Polar Stereographic, lon0=0E,
+            )
+            # X along 90E, Y along 0E
+            self.hemisphere = "s"  # north='n', south='s'
+
+        if name == "antarctic_ocean":
+            self.long_name = "Antarctica Ocean"
+            self.minxm = -3820e3  # bottom left corner x coordinate of grid in m
+            self.minym = -3420e3  # bottom left corner y coordinate of grid in m
+            self.grid_x_size = 7640e3  # Width of grid in integer m
+            self.grid_y_size = 6840e3  # Width of grid in integer m
+            # For lat/lon -> x,y transforms for each area
+            self.coordinate_reference_system = (
+                "epsg:3031"  # WGS 84 / Antarctic Polar Stereographic, lon0=0E,
+            )
+            # X along 90E, Y along 0E
+            self.hemisphere = "s"  # north='n', south='s'
+
+        if name == "greenland":
+            self.long_name = "Greenland"
+            self.minxm = -1000e3  # bottom left corner x coordinate of grid in m
+            self.minym = -3500e3  # bottom left corner y coordinate of grid in m
+            self.grid_x_size = 2100e3  # Width of grid in integer m
+            self.grid_y_size = 3100e3  # Width of grid in integer m
+            # For lat/lon -> x,y transforms for each area
+            self.coordinate_reference_system = (
+                "epsg:3413"  # WGS 84 / NSIDC Sea Ice Polar Stereographic North: lon0=45W,
+            )
+            # X along 45E, Y along 135E
+            self.hemisphere = "n"  # north='n', south='s'
+
+        if name == "greenland_wider_area":
+            self.long_name = "Greenland"
+            self.minxm = -972134.6133  # bottom left corner x coordinate of grid in m
+            self.minym = -3412788.5034  # bottom left corner y coordinate of grid in m
+            self.grid_x_size = 2150e3  # Width of grid in integer m
+            self.grid_y_size = 2900e3  # Width of grid in integer m
+            # For lat/lon -> x,y transforms for each area
+            self.coordinate_reference_system = (
+                "epsg:3413"  # WGS 84 / NSIDC Sea Ice Polar Stereographic North: lon0=45W,
+            )
+            # X along 45E, Y along 135E
+            self.hemisphere = "n"  # north='n', south='s'
+
+        if name == "greenland_centre":
+            self.long_name = "small area in centre of Greenland"
+            self.minxm = 102863.21  # bottom left corner x coordinate of grid in m
+            self.minym = -1962747.09  # bottom left corner y coordinate of grid in m
+            self.grid_x_size = 100e3  # Width of grid in integer m
+            self.grid_y_size = 100e3  # Width of grid in integer m
+            # For lat/lon -> x,y transforms for each area
+            self.coordinate_reference_system = (
+                "epsg:3413"  # WGS 84 / NSIDC Sea Ice Polar Stereographic North: lon0=45W,
+            )
+            # X along 45E, Y along 135E
+            self.hemisphere = "n"  # north='n', south='s'
+
+        if name == "amundsen_sea_area":
+            self.long_name = "Amundsen Sea Area"
+            self.minxm = -1767344  # bottom left corner x coordinate of grid in m
+            self.minym = -739296  # bottom left corner y coordinate of grid in m
+            self.grid_x_size = 600e3  # Width of grid in integer m
+            self.grid_y_size = 760e3  # Width of grid in integer m
+            # For lat/lon -> x,y transforms for each area
+            self.coordinate_reference_system = (
+                "epsg:3031"  # WGS 84 / Antarctic Polar Stereographic, lon0=0E,
+            )
+            # X along 90E, Y along 0E
+            self.hemisphere = "s"  # north='n', south='s'
+
+        if name == "spirit_all":
+            self.long_name = "SPIRIT all"
+            self.minxm = 1288489  # bottom left corner x coordinate of grid in m
+            self.minym = -2231728  # bottom left corner y coordinate of grid in m
+            self.grid_x_size = 697e3  # Width of grid in integer m
+            self.grid_y_size = 500e3  # Width of grid in integer m
+            # For lat/lon -> x,y transforms for each area
+            self.coordinate_reference_system = (
+                "epsg:3031"  # WGS 84 / Antarctic Polar Stereographic, lon0=0E,
+            )
+            # X along 90E, Y along 0E
+            self.hemisphere = "s"  # north='n', south='s'
+
+        if name == "ross_1600km":
+            self.long_name = "Ross Ice Shelf Area"
+            self.minxm = -799999.99  # bottom left corner x coordinate of grid in m
+            self.minym = -1670577.355  # bottom left corner y coordinate of grid in m
+            self.grid_x_size = 1600e3  # Width of grid in integer m
+            self.grid_y_size = 1600e3  # Width of grid in integer m
+            # For lat/lon -> x,y transforms for each area
+            self.coordinate_reference_system = (
+                "epsg:3031"  # WGS 84 / Antarctic Polar Stereographic, lon0=0E,
+            )
+            # X along 90E, Y along 0E
+            self.hemisphere = "s"  # north='n', south='s'
+
+        if name == "filchner_1300km":
+            self.long_name = "Filchner Ice Shelf Area"
+            self.minxm = -1688115.9815  # bottom left corner x coordinate of grid in m
+            self.minym = -50643.4586  # bottom left corner y coordinate of grid in m
+            self.grid_x_size = 1300e3  # Width of grid in integer m
+            self.grid_y_size = 1300e3  # Width of grid in integer m
+            # For lat/lon -> x,y transforms for each area
+            self.coordinate_reference_system = (
+                "epsg:3031"  # WGS 84 / Antarctic Polar Stereographic, lon0=0E,
+            )
+            # X along 90E, Y along 0E
+            self.hemisphere = "s"  # north='n', south='s'
+
+        if (
+            name == "arctic_russia"
+        ):  # Covers Svalbard, Franz Josef Land, Novaya Zemlya and Severnaya Zemlya
+            self.long_name = "Arctic Russia Area"
+            self.minxm = 500000.0  # bottom left corner x coordinate of grid in m
+            self.minym = -800000.0  # bottom left corner y coordinate of grid in m
+            self.grid_x_size = 1400000.0  # Width of grid in integer m
+            self.grid_y_size = 1900000.0  # Width of grid in integer m
+            # For lat/lon -> x,y transforms for each area
+            self.coordinate_reference_system = (
+                "epsg:3413"  # WGS 84 / NSIDC Sea Ice Polar Stereographic North: lon0=45W,
+            )
+            # X along 45E, Y along 135E
+            self.hemisphere = "n"  # north='n', south='s'
+
+        if name == "svalbard":  # Svalbard
+            self.long_name = "Svalbard Area"
+            self.minxm = 900000.0  # bottom left corner x coordinate of grid in m
+            self.minym = -800000.0  # bottom left corner y coordinate of grid in m
+            self.grid_x_size = 450000.0  # Width of grid in integer m
+            self.grid_y_size = 600000.0  # Width of grid in integer m
+            # For lat/lon -> x,y transforms for each area
+            self.coordinate_reference_system = (
+                "epsg:3413"  # WGS 84 / NSIDC Sea Ice Polar Stereographic North: lon0=45W,
+            )
+            # X along 45E, Y along 135E
+            self.hemisphere = "n"  # north='n', south='s'
+
+        if name == "novaya_zemlya":  # Novaya Zemlya
+            self.long_name = "Novaya Zemlya Area"
+            self.minxm = 1250000.0  # bottom left corner x coordinate of grid in m
+            self.minym = 150000.0  # bottom left corner y coordinate of grid in m
+            self.grid_x_size = 650000.0  # Width of grid in integer m
+            self.grid_y_size = 500000.0  # Width of grid in integer m
+            # For lat/lon -> x,y transforms for each area
+            self.coordinate_reference_system = (
+                "epsg:3413"  # WGS 84 / NSIDC Sea Ice Polar Stereographic North: lon0=45W,
+            )
+            # X along 45E, Y along 135E
+            self.hemisphere = "n"  # north='n', south='s'
+
+        if name == "franz_josef_land":  # Franz Josef Land
+            self.long_name = "Franz Josef Land Area"
+            self.minxm = 800000.0  # bottom left corner x coordinate of grid in m
+            self.minym = -50000.0  # bottom left corner y coordinate of grid in m
+            self.grid_x_size = 350000.0  # Width of grid in integer m
+            self.grid_y_size = 450000.0  # Width of grid in integer m
+            # For lat/lon -> x,y transforms for each area
+            self.coordinate_reference_system = (
+                "epsg:3413"  # WGS 84 / NSIDC Sea Ice Polar Stereographic North: lon0=45W,
+            )
+            # X along 45E, Y along 135E
+            self.hemisphere = "n"  # north='n', south='s'
+
+        if name == "severnaya_zemlya":  # Severnaya Zemlya
+            self.long_name = "Severnaya Zemlya Area"
+            self.minxm = 550000.0  # bottom left corner x coordinate of grid in m
+            self.minym = 650000.0  # bottom left corner y coordinate of grid in m
+            self.grid_x_size = 250000.0  # Width of grid in integer m
+            self.grid_y_size = 450000.0  # Width of grid in integer m
+            # For lat/lon -> x,y transforms for each area
+            self.coordinate_reference_system = (
+                "epsg:3413"  # WGS 84 / NSIDC Sea Ice Polar Stereographic North:
+            )
+            # lon0=45W, X along 45E, Y along 135E
+            self.hemisphere = "n"  # north='n', south='s'
+
+        if name == "arctic":  # Arctic
+            self.long_name = "Arctic Area"
+            self.minxm = -3850000.0  # bottom left corner x coordinate of grid in m
+            self.minym = -5350000.0  # bottom left corner y coordinate of grid in m
+            self.grid_x_size = 7600000.0  # Width of grid in integer m
+            self.grid_y_size = 11200000.0  # Height of grid in integer m
+            # For lat/lon -> x,y transforms for each area
+            self.coordinate_reference_system = "epsg:3413"  # WGS 84 / NSIDC Sea Ice Polar
+            # Stereographic North: lon0=45W, X along 45E, Y along 135E
+            self.hemisphere = "n"  # north='n', south='s'
+
+        if name == "chongtar":  # Chongtar surging glacier region of high moutain Asia
+            self.long_name = "Chongtar Area"
+            self.minxm = 6125000.0  # bottom left corner x coordinate of grid in m
+            self.minym = -1541000.0  # bottom left corner y coordinate of grid in m
+            self.grid_x_size = 67000.0  # Width of grid in integer m
+            self.grid_y_size = 68000.0  # Height of grid in integer m
+            # For lat/lon -> x,y transforms for each area
+            self.coordinate_reference_system = (
+                "epsg:3995"  # WGS 84 / NSIDC Sea Ice Polar Stereographic North: lon0=0E lat0=71N
+            )
+            self.hemisphere = "n"  # north='n', south='s'
+
+        self.maxxm = self.minxm + self.grid_x_size
+        self.maxym = self.minym + self.grid_y_size
+        # create mesh grid
+
+        # make x,y cell centre coordinate arrays. Note these are centre coordinates so,
+        # start half a bin in from the lower left corner,
+        # and stop half bin from the maximum extent
+        self.cell_x_centres = np.arange(
+            self.minxm + binsize / 2.0, self.maxxm, binsize
+        )  # start, stop, step : stop value not included
+        self.cell_y_centres = np.arange(self.minym + binsize / 2.0, self.maxym, binsize)
+
+        # Create a grid mesh of ccordinates
+        self.xmesh, self.ymesh = np.meshgrid(self.cell_x_centres, self.cell_y_centres)
+
+        # setup coordinate reference system (crs) for this projection
+        self.crs_bng = CRS(self.coordinate_reference_system)
+
+        # Setup the Transforms
+        self.xy_to_lonlat_transformer = Transformer.from_proj(
+            self.crs_bng, self.crs_wgs, always_xy=True
+        )
+        self.lonlat_to_xy_transformer = Transformer.from_proj(
+            self.crs_wgs, self.crs_bng, always_xy=True
+        )
+
+    def info(self):
+        """print info on the class instance"""
+        print("Grid Name : ", self.name)
+        print("    Grid Long Name : ", self.long_name)
+        print("    Grid CRS : ", self.coordinate_reference_system)
+        print("    Width (in X) : ", self.grid_x_size, "(m)")
+        print("    Height (in Y) : ", self.grid_y_size, "(m)")
+        print("    Bottom left coordinates (min x,y): ", self.minxm, self.minym, "(m)")
+        print("Binsize", self.binsize, " (m)")
+        print("Ncols, Nrows=", self.get_ncols_nrows())
+
+    def get_ncols_nrows(self):
+        """
+        returns the number of grid columns (x) and rows (y) for a given bin size in m
+        """
+
+        ncols = int(self.grid_x_size / self.binsize)
+        nrows = int(self.grid_y_size / self.binsize)
+        return ncols, nrows
+
+    def get_col_row_from_x_y(self, x, y):
+        """
+        returns the grid column and row for a given x,y and bin size in m
+        """
+        ii = int(np.floor((x - self.minxm) / self.binsize))
+        jj = int(np.floor((y - self.minym) / self.binsize))
+        return ii, jj
+
+    def get_cellcentre_x_y_from_col_row(self, col, row):
+        """
+        returns cell centre x,y in (m) from the grid column and row indices and bin size
+        """
+        x = self.minxm + col * self.binsize + (self.halfbinsize)
+        y = self.minym + row * self.binsize + (self.halfbinsize)
+
+        return x, y
+
+    def get_cellcentre_lat_lon_from_col_row(self, col, row):
+        """
+        returns latitude and longitude E of cell centre from the grid column and row indices
+        and bin size
+        """
+        x = self.minxm + col * self.binsize + (self.halfbinsize)
+        y = self.minym + row * self.binsize + (self.halfbinsize)
+
+        # transform to lat, lon
+
+        lon, lat = self.xy_to_lonlat_transformer.transform(x, y)
+        lon = np.mod(lon, 360)
+
+        return lat, lon
+
+    def get_xy_relative_to_cellcentre(self, x, y, col, row):
+        """
+        returns  the offset in x and y from grid cell (ncol,nrow) centre
+        """
+
+        return x - (self.minxm + col * self.binsize + (self.halfbinsize)), y - (
+            self.minym + row * self.binsize + (self.halfbinsize)
+        )
+
+    def transform_x_y_to_lat_lon(self, x, y):
+        """
+        returns latitude and longitude E of cell centre from the arrays of x,y
+        """
+        lon, lat = self.xy_to_lonlat_transformer.transform(x, y)
+        lon = np.mod(lon, 360)
+
+        return lat, lon
+
+    def transform_lat_lon_to_x_y(self, lats, lons):
+        """
+        returns x,y in grid projection of latitude and longitude E
+        """
+
+        return self.lonlat_to_xy_transformer.transform(lons, lats)
---
+++ src/cpom/gridding/gridding.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Module for irregular data gridding functions optimised for typical satellite data uses
+
+Author: Alan Muir (CPOM/UCL)
+Date: 2019
+Copyright: UCL/MSSL/CPOM. Not to be used outside CPOM/MSSL without permission of author 
+
+"""
+
+import sys
+from math import floor
+
+# Package Imports
+import numpy as np
+
+# pylint: disable=too-many-branches,too-many-locals,too-many-arguments
+# ----------------------------------------------------------------------------------------------
+# 	griddata
+# 		bin irregular data (x,y,vals) in to a 2d rectangular grid of cell size: binsize in meters.
+# 		grid coordinates defined by x,y extrema : origin xmin-binsize/2., ymin - binsize/2.
+# 		ie xmin,ymin is in center of 1st bin (not edge).
+# 		Calculates median (or mean or %failure) of binned values. Nans are allowed but excluded
+#       from calculation.
+#
+# 		returns: 2d grid of calculated (median, mean or %failure)
+# ----------------------------------------------------------------------------------------------
+
+
+def griddata(
+    x,
+    y,
+    z,
+    binsize=10000,
+    calc_median=False,
+    calc_mean=False,
+    calc_percent_of_value=False,
+    return_density=False,
+):
+    """
+    bin irregular data (x,y,vals) in to a 2d rectangular grid of cell size: binsize in meters.
+    grid coordinates defined by x,y extrema : origin xmin-binsize/2., ymin - binsize/2.
+    ie xmin,ymin is in center of 1st bin (not edge).
+    Calculates median (or mean or %value) of binned values.
+    Nans are allowed but excluded from calculation.
+    Empty bins are set to np.nan
+
+    Parameters
+    ----------
+    x : ndarray (1D)
+        float, x values (in meters).
+    y : ndarray (1D)
+        float, y values (in meters).
+    z : ndarray (1D)
+        corresponding data values.
+    binsize : scalar, optional, units meters
+        The full width and height of each bin on the grid.  If each
+        bin is a cube, then this is the x and y dimension.  This is
+        the step in both directions, x and y. Defaults to 10000 (ie 10km)
+    calc_median : boolean, default=False. Calculate the median value in each bin
+    as return grid value
+    calc_mean : boolean, default=False. Calculate the mean value in each bin
+    as return grid value
+    calc_percent_of_value : a scalar value or boolean False, default=False.
+    Calculate the % of this value in each bin as return grid value
+
+    Returns
+    -------
+    grid : ndarray (2D)
+        The evenly gridded data.  The value of each cell is the median,
+        mean or %failure value of
+        the contents of the bin.
+    density_grid : ndarray (2D) of int, optional
+        xi, yi: the mesh of x,y coordinates for new grid
+         grid of measurement density (number of measurements in each cell)
+         only returned if return_density==True
+
+    Revisions
+    ---------
+    2019-11-04  Initial version
+
+    """
+
+    if not calc_mean and not calc_median and not calc_percent_of_value:
+        sys.exit("griddata: Must set either calc_mean, calc_median, calc_percent_of_value to True")
+
+        # get extrema values of input x,y coords
+    xmin, xmax = x.min(), x.max()
+    ymin, ymax = y.min(), y.max()
+
+    # make x,y coordinate arrays.
+    xi = np.arange(xmin - binsize / 2.0, xmax + binsize / 2.0, binsize)
+    yi = np.arange(ymin - binsize / 2.0, ymax + binsize / 2.0, binsize)
+    ximin = xmin - binsize / 2.0
+    yimin = ymin - binsize / 2.0
+    xi, yi = np.meshgrid(xi, yi)
+
+    # create the output grid.
+    grid = np.full(xi.shape, np.nan, dtype=x.dtype)
+    if return_density:
+        density_grid = np.full(xi.shape, np.nan, dtype=x.dtype)
+    nrow, ncol = grid.shape
+
+    # Go through the data and fill in the grid
+
+    # Create a grid of empty lists (ie each index contains [None])
+    bins = [[[None] for x in range(ncol)] for y in range(nrow)]
+
+    if calc_median:
+        print("filling grid with median values")
+    if calc_mean:
+        print("filling grid with mean values")
+    if calc_percent_of_value:
+        print("filling grid with % of ", calc_percent_of_value)
+
+    # Fill the grid with input data points z
+    for i in range(z.size):
+        xtmp = x[i]
+        ytmp = y[i]
+        grid_x_pos = floor((xtmp - ximin) / binsize)
+        grid_y_pos = floor((ytmp - yimin) / binsize)
+
+        if not bins[grid_y_pos][grid_x_pos][0]:
+            bins[grid_y_pos][grid_x_pos][0] = z[i]
+        else:
+            bins[grid_y_pos][grid_x_pos].append(z[i])
+
+    # Calculate the grid statistic to be output (ie mean. median, % of value)
+    for row in range(nrow):
+        for col in range(ncol):
+            if bins[row][col][0] is None:
+                grid[row, col] = np.nan
+                continue
+            if calc_median:
+                grid[row, col] = np.nanmedian(bins[row][col])
+            if calc_mean:
+                grid[row, col] = np.nanmean(bins[row][col])
+            if calc_percent_of_value:
+                vals = np.array(bins[row][col])
+                w = np.where(vals == calc_percent_of_value)[0]
+                grid[row, col] = 100.0 * w.size / vals.size
+            if return_density:
+                density_grid[row, col] = len(bins[row][col])
+                # print(row, col, density_grid[row,col])
+    if return_density:
+        return grid, xi, yi, density_grid
+    return grid, xi, yi
