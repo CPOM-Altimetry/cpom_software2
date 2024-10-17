73ccda60830e2f47d43f2712ffb6b5db3edc891b
Alan Muir <a.muir@ucl.ac.uk>
2024-10-17 11:41:25 +0100

    added src/cpom/gridding/__init__.py

---
+++ src/cpom/gridding/__init__.py
@@ -0,0 +1,10 @@
+"""
+# Modules to Support Gridding
+
+gridareas.py: provides class GridArea: class to define and use named grid areas for polar regions
+
+gridding.py: provides function griddata() to bin irregular data (x,y,vals) in to a 
+             2d rectangular grid of cell size: binsize in meters
+
+
+"""
