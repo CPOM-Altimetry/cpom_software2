3668815fb1f9aaf5bb9ee6a93deb1ba3818ece1d
Alan Muir <a.muir@ucl.ac.uk>
2024-12-12 18:18:12 +0000

    cpom.areas docs update

--- src/cpom/areas/__init__.py
+++ src/cpom/areas/__init__.py
@@ -1,4 +1,19 @@
 """cpom.areas
 
 # CPOM Area Definitions and Polarplot class
+
+## definitions  
+
+contain standard area definition files used by the Polarplot class and related tools 
+such as plot_map.py. Each area definition is stored in a separate <area_name>.py file.
+
+## areas.py
+
+contains code to handle the area definitions
+
+## area_plot.py
+
+contains the Polarplot class. The main external function of the Polarplot class
+is  Polarplot(..).plot_area(..)
+
 """
