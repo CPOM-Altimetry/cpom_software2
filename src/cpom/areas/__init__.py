9e6fc2eca2cfeb8a83de6dd2c7aa2e828bd9a80c
Alan Muir <a.muir@ucl.ac.uk>
2024-12-13 17:05:39 +0000

    docs update

--- src/cpom/areas/__init__.py
+++ src/cpom/areas/__init__.py
@@ -14,8 +14,43 @@ contains code to handle the area definitions
 ## area_plot.py
 
 contains the Polarplot class. The main external function of the Polarplot class
-is  Polarplot(..).plot_points(..)
+is:
 
 `cpom.areas.area_plot.Polarplot.plot_points`
+
+## Examples of using Polarplot().plot_points()
+
+The purpose of Polarplot('some_area_name').plot_points() is to plot latitude, longitude, 
+and values on predefined area maps. plot_points() takes one or more dataset dictionaries as input, 
+containing the lat,lon,values and associated parameters.
+
+The following example plots 2 data sets on a basic map of Antarctica:
+
+```
+import numpy as np
+from cpom.areas.area_plot import Polarplot
+
+dataset1={
+    "lats": np.linspace(-80,-70,100),
+    "lons": np.linspace(0,5,100),
+    "vals": np.linspace(0,10,100),
+    "units": 'm',
+    "name": 'test_param',
+    "valid_range": [0,5],
+}
+
+dataset2={
+    "lats": np.linspace(-80,-70,100),
+    "lons": np.linspace(20,25,100),
+    "vals": np.linspace(0,10,100),
+    "name": 'test_param',
+    "cmap_name": 'viridis',
+}
+
+Polarplot('antarctica').plot_points(dataset1,dataset2)
+
+```
+
+`![](/cpom_software2/images/plot_points_example1.jpeg "")`
 
 """
