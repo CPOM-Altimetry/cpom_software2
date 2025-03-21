d05de1524b4c37fd573161218ea8967fe3019f33
Alan Muir <a.muir@ucl.ac.uk>
2025-03-21 14:46:35 +0000

    added altimetry/tools/nc_vals.py tools

--- src/cpom/altimetry/tools/__init__.py
+++ src/cpom/altimetry/tools/__init__.py
@@ -16,6 +16,12 @@ that contain lat/lon locations within a CPOM Area's
 mask or within a radius (km) of a specified lat,lon point. Optionally plot tracks 
 in area map. 
 
+
+## nc_vals.py
+
+Tool which prints netcdf parameters. A bit like ncdump but with more options
+and allows single parameters to be printed more easily.
+
 Further details at `cpom.altimetry.tools.find_files_in_area`
 
 
