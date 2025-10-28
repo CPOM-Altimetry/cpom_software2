17aeaa71d81fb98b5a0f391e22110b8948e9da9d
Alan Muir <a.muir@ucl.ac.uk>
2025-10-28 15:32:06 +0000

    add transfer_mm_videos_to_portal.sh

--- src/cpom/altimetry/projects/ais_cci_plus_phase2/transfer_mm_videos_to_portal.sh
+++ src/cpom/altimetry/projects/ais_cci_plus_phase2/transfer_mm_videos_to_portal.sh
@@ -2,7 +2,8 @@
 set -euo pipefail
 
 
-rsync -av --dry-run -e "ssh -o ProxyJump=ucasamu@ssh-gateway.ucl.ac.uk" \
+rsync -av --ignore-existing -e "ssh -o ProxyJump=ucasamu@ssh-gateway.ucl.ac.uk" \
 /cpnet/altimetry/landice/ais_cci_plus_phase2/sec_processing/multimission/mm_quicklooks/ \
 asm@li1.cpom.ucl.ac.uk:/cpnet/www/cpom/ais_cci_phase2/multi_mission_quicklooks
+
 
