cd3aa69b1042c3c1296fc1ce04ab9feed8f4e141
Alan Muir <a.muir@ucl.ac.uk>
2025-11-04 17:19:26 +0000

    add altimetry/projects/ais_cci_plus_phase2/transfer_single_mission_products.sh

--- src/cpom/altimetry/projects/ais_cci_plus_phase2/transfer_single_mission_products.sh
+++ src/cpom/altimetry/projects/ais_cci_plus_phase2/transfer_single_mission_products.sh
@@ -3,8 +3,10 @@ set -euo pipefail
 
 prod_dir=/cpnet/altimetry/landice/ais_cci_plus_phase2/products/single_mission
 
+rm -f ${prod_dir}/ESACCI*.nc
+
 rsync  -e "ssh -i ~/.ssh/id_rsa_northumbria -o ProxyCommand='ssh -i ~/.ssh/id_rsa_northumbria -W %h:%p alanmuir@138.248.197.3 -p 10999'" \
- -avz --no-t --no-perms --dry-run \
+ -avz --no-t --no-perms \
  alanmuir@10.0.0.24:${prod_dir}/*.nc \
  $prod_dir
 
