50d03462e2e7cfd62e9586e8068d6e8c110e90a7
Alan Muir <a.muir@ucl.ac.uk>
2025-11-06 10:45:09 +0000

    update to sync_to_portal.sh

--- src/cpom/altimetry/projects/ais_cci_plus_phase2/sync_to_portal.sh
+++ src/cpom/altimetry/projects/ais_cci_plus_phase2/sync_to_portal.sh
@@ -37,4 +37,29 @@ rm -f /cpnet/www/cpom/ais_cci_phase2/multi_mission_quicklooks/*
  -avz --no-t --no-perms \
  alanmuir@10.0.0.24:${mm_video_dir}/ \
  $portal_dir
+
+# Sync multi-mission products
+nu_prod_dir=/cpnet/altimetry/landice/ais_cci_plus_phase2/sec_processing/multimission/products
+ucl_prod_dir=/cpnet/altimetry/landice/ais_cci_plus_phase2/products/multi_mission
+
+rm -f $ucl_prod_dir/*.zip
+rm -f $ucl_prod_dir/*.nc
+
+# Get the .zip file (containing all the multi-mission files)
+/usr/bin/rsync  -e "ssh -i ~/.ssh/id_rsa_northumbria -o ProxyCommand='ssh -i ~/.ssh/id_rsa_northumbria -W %h:%p alanmuir@138.248.197.3 -p 10999'" \
+ -avz  \
+ alanmuir@10.0.0.24:${nu_prod_dir}/*.zip \
+ $ucl_prod_dir
+ 
+ # choose newest ESA*.nc on the remote, then rsync just that one
+ssh -i ~/.ssh/id_rsa_northumbria \
+    -o ProxyCommand='ssh -i ~/.ssh/id_rsa_northumbria -W %h:%p alanmuir@138.248.197.3 -p 10999' \
+    alanmuir@10.0.0.24 \
+    "find $(printf %q "${nu_prod_dir}") -maxdepth 1 -name 'ESA*.nc' -printf '%T@ %p\n' \
+     | sort -n | tail -n 1 | cut -d' ' -f2-" \
+| xargs -I{} /usr/bin/rsync \
+    -e "ssh -i ~/.ssh/id_rsa_northumbria -o ProxyCommand='ssh -i ~/.ssh/id_rsa_northumbria -W %h:%p alanmuir@138.248.197.3 -p 10999'" \
+    -avz "alanmuir@10.0.0.24:{}" "${ucl_prod_dir}"
+
+
  
