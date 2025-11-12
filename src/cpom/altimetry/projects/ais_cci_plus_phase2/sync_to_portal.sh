0bcdc059e7aff4d446347fbd511539725be453c0
Alan Muir <a.muir@ucl.ac.uk>
2025-11-12 13:00:41 +0000

    add gen_annual_dh_videos.sh

--- src/cpom/altimetry/projects/ais_cci_plus_phase2/sync_to_portal.sh
+++ src/cpom/altimetry/projects/ais_cci_plus_phase2/sync_to_portal.sh
@@ -4,9 +4,27 @@ set -euo pipefail
 # Sync all AIS CCI content to the UCL portal
 # - single mission products and quicklooks
 # - multi-mission videos
+# - annual dh videos
 
+
+#---------------------------------------------------------------------------------
+# Transdfer annual dh videos from NU to UCL
+# this script must be called from UCL
+#---------------------------------------------------------------------------------
+
+dh_video_dir=/cpnet/altimetry/landice/ais_cci_plus_phase2/products/annual_dh/videos 
+portal_dir=/cpnet/www/cpom/ais_cci_phase2/annual_dh_quicklooks
+rm -f /cpnet/www/cpom/ais_cci_phase2/annual_dh_quicklooks/*
+/usr/bin/rsync  -e "ssh -i ~/.ssh/id_rsa_northumbria -o ProxyCommand='ssh -i ~/.ssh/id_rsa_northumbria -W %h:%p alanmuir@138.248.197.3 -p 10999'" \
+ -avz --no-t --no-perms \
+ alanmuir@10.0.0.24:${dh_video_dir}/ \
+ $portal_dir    
+
+exit 1
+#---------------------------------------------------------------------------------
 # Transfers single mission CCI products from NU to UCL
 # this script must be called from UCL
+#---------------------------------------------------------------------------------
 
 prod_dir=/cpnet/altimetry/landice/ais_cci_plus_phase2/products/single_mission
 
@@ -23,7 +41,9 @@ rm -f ${portal_dir}/*
 mv ${prod_dir}/*.webp  ${portal_dir}
 mv ${prod_dir}/*.avif  ${portal_dir}
 
+#---------------------------------------------------------------------------------
 # Sync multi-mission videos to the portal
+#---------------------------------------------------------------------------------
 
 # at NU
 mm_video_dir=/cpnet/altimetry/landice/ais_cci_plus_phase2/products/multi_mission/videos
@@ -38,7 +58,10 @@ rm -f /cpnet/www/cpom/ais_cci_phase2/multi_mission_quicklooks/*
  alanmuir@10.0.0.24:${mm_video_dir}/ \
  $portal_dir
 
+#---------------------------------------------------------------------------------
 # Sync multi-mission products
+#---------------------------------------------------------------------------------
+
 nu_prod_dir=/cpnet/altimetry/landice/ais_cci_plus_phase2/products/multi_mission
 ucl_prod_dir=/cpnet/altimetry/landice/ais_cci_plus_phase2/products/multi_mission
 
