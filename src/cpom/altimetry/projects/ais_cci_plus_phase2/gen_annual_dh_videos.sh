14661eaf3acd3585f1aedf73a5ca234f6ddcc1b9
Alan Muir <a.muir@ucl.ac.uk>
2025-11-12 14:15:32 +0000

    gen_annual_videos.sh update

--- src/cpom/altimetry/projects/ais_cci_plus_phase2/gen_annual_dh_videos.sh
+++ src/cpom/altimetry/projects/ais_cci_plus_phase2/gen_annual_dh_videos.sh
@@ -9,65 +9,66 @@ rm -f videos/*
 for type in dh-ase dh
 do
 ffmpeg \
--framerate 12 \
--pattern_type glob -i "quicklooks/*${type}.webp" \
--c:v libaom-av1 \
--cpu-used 3 -row-mt 1 -tiles 2x1 -lag-in-frames 48 -aq-mode 1  \
--crf 34 \
--b:v 0 \
--g 24 \
--keyint_min 24 \
--pix_fmt yuv420p \
-videos/annual_dh_av1.${type}.webm
+  -framerate 2 \
+  -pattern_type glob -i "quicklooks/*${type}.webp" \
+  -c:v libaom-av1 \
+  -cpu-used 4 -row-mt 0 -tiles 1x1 -lag-in-frames 16 -aq-mode 1 \
+  -crf 22 \
+  -b:v 0 \
+  -g 2 \
+  -keyint_min 2 \
+  -pix_fmt yuv420p \
+  videos/annual_dh_av1.${type}.webm
 
 # Now generate AV1 with hillshade
 ffmpeg \
--framerate 12 \
+-framerate 2 \
 -pattern_type glob -i "quicklooks/*${type}-hs.webp" \
 -c:v libaom-av1 \
--cpu-used 3 -row-mt 1 -tiles 2x1 -lag-in-frames 48 -aq-mode 1  \
--crf 34 \
+-cpu-used 4 -row-mt 0 -tiles 1x1 -lag-in-frames 16 -aq-mode 1 \
+-crf 22 \
 -b:v 0 \
--g 24 \
--keyint_min 24 \
+-g 2 \
+-keyint_min 2 \
 -pix_fmt yuv420p \
 videos/annual_dh_av1_hs.${type}.webm
 
 # Generate VP9 format 
 
 ffmpeg \
--framerate 12 \
+-framerate 2 \
 -pattern_type glob -i "quicklooks/*${type}.webp" \
 -c:v libvpx-vp9  \
--crf 32 \
+-crf 20 \
 -b:v 0 \
--g 24 \
--keyint_min 24 \
+-g 2 \
+-keyint_min 2 \
 -pix_fmt yuv420p \
 videos/annual_dh_vp9.${type}.webm
 
 # Now generate VP9 format with hillshade
 
 ffmpeg \
--framerate 12 \
+-framerate 2 \
 -pattern_type glob -i "quicklooks/*${type}-hs.webp" \
 -c:v libvpx-vp9  \
--crf 32 \
+-crf 20 \
 -b:v 0 \
--g 24 \
--keyint_min 24 \
+-g 2 \
+-keyint_min 2 \
 -pix_fmt yuv420p \
 videos/annual_dh_vp9_hs.${type}.webm
 
 # Generate MP4 format 
 
 ffmpeg \
--framerate 12 \
+-framerate 2 \
 -pattern_type glob -i "quicklooks/*${type}.webp" \
 -c:v libx264 \
--crf 23 \
--g 24 \
--keyint_min 24 \
+-preset slow -tune stillimage \
+-crf 19 \
+-g 2 \
+-keyint_min 2 \
 -pix_fmt yuv420p \
 -movflags +faststart \
 videos/annual_dh_h264.${type}.mp4
@@ -75,12 +76,13 @@ videos/annual_dh_h264.${type}.mp4
 # Generate MP4 format with hillshade
 
 ffmpeg \
--framerate 12 \
+-framerate 2 \
 -pattern_type glob -i "quicklooks/*${type}-hs.webp" \
 -c:v libx264 \
--crf 23 \
--g 24 \
--keyint_min 24 \
+-preset slow -tune stillimage \
+-crf 19 \
+-g 2 \
+-keyint_min 2 \
 -pix_fmt yuv420p \
 -movflags +faststart \
 videos/annual_dh_h264_hs.${type}.mp4
