48b8940624f6272b873a219961124bf4200a36de
williscr <clarewillis.79@outlook.com>
2026-04-29 10:55:28 +0000

    add missing init

--- src/cpom/altimetry/tools/__init__.py
+++ src/cpom/altimetry/tools/__init__.py
@@ -11,10 +11,10 @@ Further details at `cpom.altimetry.tools.plot_map`
 
 ## find_files_in_area.py
 
-Tool to to find files within a specified directory (and default is to search recursive sub-dirs) 
+Tool to to find files within a specified directory (and default is to search recursive sub-dirs)
 that contain lat/lon locations within a CPOM Area's
-mask or within a radius (km) of a specified lat,lon point. Optionally plot tracks 
-in area map. 
+mask or within a radius (km) of a specified lat,lon point. Optionally plot tracks
+in area map.
 
 Further details at `cpom.altimetry.tools.find_files_in_area`
 
@@ -27,8 +27,8 @@ Further details at `cpom.altimetry.tools.nc_vals`
 
 ## validate_l2_altimetry_elevations.py
 
-This tool compares a selected month of radar altimetry mission elevation data 
-against laser altimetry reference elevations. 
+This tool compares a selected month of radar altimetry mission elevation data
+against laser altimetry reference elevations.
 It also allows comparing a reference mission to itself.
 
 Further details at `cpom.altimetry.tools.validate_l2_altimetry_elevations`
