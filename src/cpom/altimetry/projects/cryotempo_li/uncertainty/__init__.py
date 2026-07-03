8cda418f65478bc7f008a0a065208cc69bb1e8ba
Alan Muir <a.muir@ucl.ac.uk>
2026-07-03 11:03:23 +0100

    added 3d/4d uncertainty scripts for CryoTEMPO

--- src/cpom/altimetry/projects/cryotempo_li/uncertainty/__init__.py
+++ src/cpom/altimetry/projects/cryotempo_li/uncertainty/__init__.py
@@ -1,3 +1,9 @@
 """
 # Uncertainty code for CryoTEMPO LI
+
+Includes the original 2D (slope, roughness) uncertainty table/fit scripts
+(cryotempo_2d_*.py) and the scripts to generate the CryoTEMPO-minus-ICESat-2
+point-to-point difference inputs for the 3D/4D elevation-uncertainty LUT builders
+(run_is2_diffs_2020.sh + aggregate_p2p_diffs.py), adapted from the CLEV2ER landice
+method in cpom.altimetry.projects.clev2er_liiw.uncertainty.
 """
